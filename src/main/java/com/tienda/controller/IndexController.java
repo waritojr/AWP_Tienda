6b321e1cd80df6678d1e03f31d244813760e0158
waritojr <mario.cv2002@gmail.com>
2022-06-17 10:59:18 -0600

    Semana 5 de Aplicaciones Web y Patrones (Spring Hibernate CRUD)

--- src/main/java/com/tienda/controller/IndexController.java
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.tienda.controller;
 
 import com.tienda.dao.ClienteDao;
 import com.tienda.domain.Cliente;
+import com.tienda.service.ClienteService;
 import java.util.Arrays;
 import lombok.var;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
@@ -16,26 +18,57 @@ public class IndexController {
 
     // Se crea en tiempo de ejecucion si aun no se ha creado...
     @Autowired
-    private ClienteDao clienteDao;
-    
+    private ClienteService clienteService;
+
     @GetMapping("/")
     public String inicio(Model model) {
-        
-        log.info("Ahora se usa arquitectura MVC");
-        
+
+        log.info("Ahora SI se usa arquitectura MVC");
+
         /*
         Cliente cliente1 = new Cliente("Pedro","Contreras Abarca","pabarca@gmail.com","8989-9898");
         Cliente cliente2 = new Cliente("Rebeca","Arguedas Alfaro","rb@gmail.com","8489-9898");
         Cliente cliente3 = new Cliente("Juan","Dominguez","jdom@gmail.com","8389-9898");
         
         var clientes = Arrays.asList(cliente1,cliente2,cliente3);
-        */
-        
+         */
         // "FindAll" Va a leer todo el cursor y lo devuelve en un ArrayList
-        var clientes = clienteDao.findAll();
-        
-        model.addAttribute("clientes",clientes);
-        
+        var clientes = clienteService.getClientes();
+
+        model.addAttribute("clientes", clientes);
+
         return "index";
+
     }
+
+    @GetMapping("/nuevoCliente")
+    public String nuevoCliente(Cliente cliente) {
+
+        return "modificarCliente";
+    }
+
+    @PostMapping("/guardarCliente")
+    public String guardarCliente(Cliente cliente) {
+
+        clienteService.save(cliente);
+
+        return "redirect:/";
+    }
+    
+    @GetMapping("/modificarCliente/{idCliente}")
+    public String modificarCliente(Cliente cliente, Model model) {
+
+        cliente = clienteService.getCliente(cliente);
+        model.addAttribute(cliente);
+        return "modificarCliente";
+    }
+    
+    @GetMapping("/eliminarCliente/{idCliente}")
+    public String modificarCliente(Cliente cliente) {
+
+        clienteService.delete(cliente);
+        
+        return "redirect:/";
+    }
+    
 }
---
+++ src/main/java/com/tienda/service/ClienteServiceImpl.java
@@ -0,0 +1,48 @@
+
+package com.tienda.service;
+
+import com.tienda.dao.ClienteDao;
+import com.tienda.domain.Cliente;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteServiceImpl implements ClienteService{
+
+    // Se crea en tiempo de ejecucion si aun no se ha creado...
+    @Autowired
+    private ClienteDao clienteDao;
+    
+    @Override
+    @Transactional (readOnly = true)
+    public List<Cliente> getClientes() {
+        
+        return (List<Cliente>) clienteDao.findAll();
+    }
+
+    @Override
+    @Transactional
+    public void save(Cliente cliente) {
+        
+        clienteDao.save(cliente);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Cliente cliente) {
+        
+        clienteDao.delete(cliente);
+    }
+
+    @Override
+    @Transactional (readOnly = true)
+    public Cliente getCliente(Cliente cliente) {
+        
+        return clienteDao.findById(cliente.getIdCliente()).orElse(null);
+    }
+    
+    
+
+}
